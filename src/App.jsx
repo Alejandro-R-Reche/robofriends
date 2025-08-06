af22dca3ffe3f70bd282a003a99169763c353f4f
Alejandro-R-Reche <arodreche@gmail.com>
2025-08-05 23:14:14 -0300

    [DONE] Added scrolling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import robots from './assets/robots'
+import Scroll from './Scroll'
 
 class App extends Component {
   constructor() {
@@ -33,7 +33,9 @@ class App extends Component {
       <div className='tc'>
         <h1>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     )
   }
