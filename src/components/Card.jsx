287440c214b49fe021268ccc1c5279d7d4f76533
Alejandro-R-Reche <arodreche@gmail.com>
2025-08-05 23:53:50 -0300

    [FIX][DONE] Robots image generation.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
-const Card = ({ name, email }) => {
+const Card = ({ id, name, email }) => {
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow'>
-            <img alt='robots' src='https://robohash.org/test?200x200' />
+            <img alt='robots' src={`https://robohash.org/test${id}?200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,10 +1,11 @@
 import Card from './Card.jsx';
 
 const CardList = ({ robots }) => {
-    const cardComponent = robots.map((user, i) => {
+    const cardComponent = robots.map((robot, i) => {
         return (
             <Card
                 key={i}
+                id={i}
                 name={robots[i].name}
                 email={robots[i].email}
             />
